a93b6c7e835fab5aeb41b38a6bfc2a25995882a2
Romel Romero <romelromero123@hotmail.com>
2025-02-08 19:01:41 +0100

    Cambios del t_clientes

--- routes/ClienteRoutes.js
+++ routes/ClienteRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const clienteController = require("../controllers/ClienteController");
 const router = express.Router();
 
-router.get("/", clienteController.getAllClientes.bind(clienteController));
-router.get("/:id", clienteController.getClienteById.bind(clienteController));
-router.post("/", clienteController.createCliente.bind(clienteController));
-router.post("/login", clienteController.loginCliente.bind(clienteController));
-router.put("/:id", clienteController.updateCliente.bind(clienteController));
-router.delete("/:id", clienteController.deleteCliente.bind(clienteController));
+router.get("/", clienteController.getAllClientes);
+router.get("/:id", clienteController.getClienteById);
+router.post("/", clienteController.createCliente);
+router.post("/login", clienteController.loginCliente);
+router.put("/:id", clienteController.updateCliente);
+router.delete("/:id", clienteController.deleteCliente);
 router.get('/enviar-correo', clienteController.enviarCorreoACliente);
 
 module.exports = router;
