49e4cdee2fb01391857c6e0076e27f5153d4dbe1
Romel Romero <romelromero123@hotmail.com>
2025-02-13 11:20:29 +0100

    Cambios modelo vuelos

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
+
+
 const aeropuertoRoutes = require("./routes/AeropuertoRoutes");
 const clienteRoutes = require("./routes/ClienteRoutes");
 const vuelosRoutes = require("./routes/VueloRoutes")
+
 
 
 const app = express();
 
 // Middleware para parsear JSON
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 // Configura las rutas
